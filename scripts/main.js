4b53fb196c03d6529cef751b80aa9c9430fb8311
hosokawat <vwxyzzy@gmail.com>
2014-03-29 21:43:45 +0900

    Cellの生成方法を変更

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ SIZE = 5;
 CANVAS_WIDTH = parseInt(document.getElementById('lifegame').width / SIZE);
 
 CANVAS_HEIGHT = parseInt(document.getElementById('lifegame').height / SIZE);
+
 (function () {
 
     function Cell(r, g, b, enable) {
@@ -10,80 +11,79 @@ CANVAS_HEIGHT = parseInt(document.getElementById('lifegame').height / SIZE);
         this.g = g;
         this.b = b;
         this.enable = enable; //int 0 or 1
-    }
-    Cell.prototype.addColor = function (cell) {
-        if (cell.enable == 0) return;
-        this.r = parseInt((this.r + cell.r));
-        this.g = parseInt((this.g + cell.g));
-        this.b = parseInt((this.b + cell.b));
-    }
-    Cell.prototype.clone = function (cell) {
-        this.enable = cell.enable;
-        this.r = cell.r;
-        this.g = cell.g;
-        this.b = cell.b;
-    }
-    Cell.prototype.reset = function () {
-        this.enable = 0;
-        this.r = 0;
-        this.g = 0;
-        this.b = 0;
-    }
-    Cell.prototype.nextCell = function (map, x, y) {
-        var cnt = 0;
-        nCell = new Cell(0, 0, 0, 0);
-        if (x > 0 & y > 0) {
-            cnt += map[x - 1][y - 1].enable;
-            nCell.addColor(map[x - 1][y - 1]);
-        }
-        if (x > 0) {
-            cnt += map[x - 1][y].enable;
-            nCell.addColor(map[x - 1][y]);
-        }
-        if (x > 0 & y < map.length - 1) {
-            cnt += map[x - 1][y + 1].enable;
-            nCell.addColor(map[x - 1][y + 1]);
-        }
-        if (y > 0) {
-            cnt += map[x][y - 1].enable;
-            nCell.addColor(map[x][y - 1]);
-        }
-        if (y < map.length - 1) {
-            cnt += map[x][y + 1].enable;
-            nCell.addColor(map[x][y + 1]);
-        }
-        if (x < map[0].length - 1 & y > 0) {
-            cnt += map[x + 1][y - 1].enable;
-            nCell.addColor(map[x + 1][y - 1]);
-        }
-        if (x < map[0].length - 1) {
-            cnt += map[x + 1][y].enable;
-            nCell.addColor(map[x + 1][y]);
-        }
-        if (x < map[0].length - 1 & y < map.length - 1) {
-            cnt += map[x + 1][y + 1].enable;
-            nCell.addColor(map[x + 1][y + 1]);
-        }
-        if (this.enable == 0 & cnt == 3) {
-            nCell.enable = 1;
-            nCell.r = parseInt(nCell.r / cnt);
-            nCell.g = parseInt(nCell.g / cnt);
-            nCell.b = parseInt(nCell.b / cnt);
-        } else if ((cnt == 2 || cnt == 3) & this.enable == 1) {
-            nCell.enable = 1;
+        this.addColor = function (cell) {
+            if (cell.enable === 0) return;
+            this.r = parseInt((this.r + cell.r));
+            this.g = parseInt((this.g + cell.g));
+            this.b = parseInt((this.b + cell.b));
+        };
+        this.clone = function (cell) {
+            this.enable = cell.enable;
+            this.r = cell.r;
+            this.g = cell.g;
+            this.b = cell.b;
+        };
+        this.reset = function () {
+            this.enable = 0;
+            this.r = 0;
+            this.g = 0;
+            this.b = 0;
+        };
+        this.nextCell = function (map, x, y) {
+            var cnt = 0;
+            nCell = new Cell(0, 0, 0, 0);
+            if (x > 0 & y > 0) {
+                cnt += map[x - 1][y - 1].enable;
+                nCell.addColor(map[x - 1][y - 1]);
+            }
+            if (x > 0) {
+                cnt += map[x - 1][y].enable;
+                nCell.addColor(map[x - 1][y]);
+            }
+            if (x > 0 & y < map.length - 1) {
+                cnt += map[x - 1][y + 1].enable;
+                nCell.addColor(map[x - 1][y + 1]);
+            }
+            if (y > 0) {
+                cnt += map[x][y - 1].enable;
+                nCell.addColor(map[x][y - 1]);
+            }
+            if (y < map.length - 1) {
+                cnt += map[x][y + 1].enable;
+                nCell.addColor(map[x][y + 1]);
+            }
+            if (x < map[0].length - 1 & y > 0) {
+                cnt += map[x + 1][y - 1].enable;
+                nCell.addColor(map[x + 1][y - 1]);
+            }
+            if (x < map[0].length - 1) {
+                cnt += map[x + 1][y].enable;
+                nCell.addColor(map[x + 1][y]);
+            }
+            if (x < map[0].length - 1 & y < map.length - 1) {
+                cnt += map[x + 1][y + 1].enable;
+                nCell.addColor(map[x + 1][y + 1]);
+            }
+            if (this.enable === 0 & cnt === 3) {
+                nCell.enable = 1;
+                nCell.r = parseInt(nCell.r / cnt);
+                nCell.g = parseInt(nCell.g / cnt);
+                nCell.b = parseInt(nCell.b / cnt);
+            } else if ((cnt === 2 || cnt === 3) & this.enable == 1) {
+                nCell.enable = 1;
 
-            nCell.r = parseInt(nCell.r / cnt);
-            nCell.g = parseInt(nCell.g / cnt);
-            nCell.b = parseInt(nCell.b / cnt);
+                nCell.r = parseInt(nCell.r / cnt);
+                nCell.g = parseInt(nCell.g / cnt);
+                nCell.b = parseInt(nCell.b / cnt);
 
-        } else {
-            nCell.enable = 0;
-            nCell.reset();
-        }
+            } else {
+                nCell.enable = 0;
+                nCell.reset();
+            }
 
-        return nCell;
+            return nCell;
+        };
     }
-
 
     function random(max) {
         return parseInt(Math.random() * max);
@@ -111,30 +111,26 @@ CANVAS_HEIGHT = parseInt(document.getElementById('lifegame').height / SIZE);
 
     function tpl_Cell() {
         switch (random(3)) {
-        case 0:
-            return new Cell(100 + random(125), random(50), 100 + random(125), 1);
-            break;
-        case 1:
-            return new Cell(random(50), 100 + random(125), 100 + random(125), 1);
-
-            break;
-        case 2:
-            return new Cell(100 + random(125), 100 + random(125), random(50), 1);
-            break;
+            case 0:
+                return new Cell(100 + random(125), random(50), 100 + random(125), 1);
+            case 1:
+                return new Cell(random(50), 100 + random(125), 100 + random(125), 1);
+            case 2:
+                return new Cell(100 + random(125), 100 + random(125), random(50), 1);
         }
     }
 
     function tpl_create(cellMap, x, y) {
         switch (random(3)) {
-        case 0:
-            tpl_DN(cellMap, x, y);
-            break;
-        case 1:
-            tpl_DN(cellMap, x, y);
-            break;
-        case 0:
-            tpl_ST(cellMap, x, y);
-            break;
+            case 0:
+                tpl_DN(cellMap, x, y);
+                break;
+            case 1:
+                tpl_DN(cellMap, x, y);
+                break;
+            case 0:
+                tpl_ST(cellMap, x, y);
+                break;
         }
     }
 
@@ -213,7 +209,7 @@ CANVAS_HEIGHT = parseInt(document.getElementById('lifegame').height / SIZE);
     function draw(cellMap) {
 
         var x, y;
-        CANVAS.beginPath()
+        CANVAS.beginPath();
         for (x = 0; x < cellMap[0].length; x++) {
             for (y = 0; y < cellMap.length; y++) {
                 if (cellMap[x][y].enable == 1) {
@@ -226,7 +222,6 @@ CANVAS_HEIGHT = parseInt(document.getElementById('lifegame').height / SIZE);
             }
         }
     }
-
     var cellMap = [];
     var x, y;
     for (x = 0; x <= CANVAS_HEIGHT; x++) {
@@ -238,5 +233,4 @@ CANVAS_HEIGHT = parseInt(document.getElementById('lifegame').height / SIZE);
     }
     tpl_ST(cellMap, random(100), random(100));
     main(cellMap);
-
 })();
